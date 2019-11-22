d1c3e3d030d486ee976152fcd5530fa139b79579
sylhare <sylhare@outlook.com>
2019-11-22 16:03:24 -0500

    Refactor linter name

--- src/name_linter.py
+++ src/name_linter.py
@@ -3,12 +3,12 @@ import unicodedata
 from difflib import SequenceMatcher
 
 
-def similar(a, b):
+def similarity_ratio(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-def compare(word, other):
-    return similar(trim(word), trim(other)) > 0.5
+def is_similar(word, other):
+    return similarity_ratio(trim(word), trim(other)) > 0.6
 
 
 def remove_digit(word):
@@ -16,7 +16,8 @@ def remove_digit(word):
 
 
 def trim(word):
-    return remove_accent(remove_digit(word.replace(".", "").replace(" ", "").lower()))
+    return remove_accent(
+        remove_digit(word.replace(".", "").replace("\\", "").replace("-", "").replace(" ", "").lower()))
 
 
 def remove_accent(text):
@@ -24,3 +25,7 @@ def remove_accent(text):
     text = text.encode('ascii', 'ignore')
     text = text.decode("utf-8")
     return str(text)
+
+
+if __name__ == "__main__":
+    pass
--- tests/test_name_lint.py
+++ tests/test_name_lint.py
@@ -6,19 +6,19 @@ from src import name_linter
 class NameTest(unittest.TestCase):
 
     def test_name_with_number(self):
-        self.assertTrue(name_linter.compare("owl2", "owl"))
-        self.assertFalse(name_linter.compare("owl", "dog"))
-        self.assertFalse(name_linter.compare("owl", "dog2"))
+        self.assertTrue(name_linter.is_similar("owl2", "owl"))
+        self.assertFalse(name_linter.is_similar("owl", "dog"))
+        self.assertFalse(name_linter.is_similar("owl", "dog2"))
 
     def test_name_with_other_characters(self):
-        self.assertTrue(name_linter.compare("owlcoding", "owl.coding"))
-        self.assertTrue(name_linter.compare("dôg", "dog"))
-        self.assertTrue(name_linter.compare("owlé", "owle"))
-        self.assertTrue(name_linter.compare("owlè", "owle"))
-        self.assertFalse(name_linter.compare("owl", "dog.dog"))
+        self.assertTrue(name_linter.is_similar("owlcoding", "owl.coding"))
+        self.assertTrue(name_linter.is_similar("dôg", "dog"))
+        self.assertTrue(name_linter.is_similar("owlé", "owle"))
+        self.assertTrue(name_linter.is_similar("owlè", "owle"))
+        self.assertFalse(name_linter.is_similar("owl", "dog.dog"))
 
     def test_name_with_similar_looks(self):
-        self.assertTrue(name_linter.compare("The Coding Owl", "owl.coding1"))
-        self.assertFalse(name_linter.compare("Super Extra Dog", "Lame Under Dog"))
-        self.assertTrue(name_linter.compare("Maté Owly Code", "mowlyco1"))
-        self.assertFalse(name_linter.compare("Super Extra Dog", "Maté Owly Code"))
+        self.assertTrue(name_linter.is_similar("The Coding Owl", "owl.coding1"))
+        self.assertFalse(name_linter.is_similar("Super Extra Dog", "Lame Under Dog"))
+        self.assertTrue(name_linter.is_similar("Maté Owly Code", "mowlyco1"))
+        self.assertFalse(name_linter.is_similar("Super Extra Dog", "Maté Owly Code"))
