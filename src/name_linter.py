e797271bf493ac67cad40e2b6875c485aba3b43a
sylhare <sylhare@outlook.com>
2019-11-22 13:51:44 -0500

    Add more complex lint

--- src/name_linter.py
+++ src/name_linter.py
@@ -1,9 +1,26 @@
 import re
+import unicodedata
+from difflib import SequenceMatcher
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 
 def compare(word, other):
-    return remove_digit(word) == remove_digit(other)
+    return similar(trim(word), trim(other)) > 0.5
 
 
 def remove_digit(word):
     return re.sub(r"\d+", "", word)
+
+
+def trim(word):
+    return remove_accent(remove_digit(word.replace(".", "").replace(" ", "").lower()))
+
+
+def remove_accent(text):
+    text = unicodedata.normalize('NFD', text)
+    text = text.encode('ascii', 'ignore')
+    text = text.decode("utf-8")
+    return str(text)
--- tests/test_name_lint.py
+++ tests/test_name_lint.py
@@ -10,4 +10,15 @@ class NameTest(unittest.TestCase):
         self.assertFalse(name_linter.compare("owl", "dog"))
         self.assertFalse(name_linter.compare("owl", "dog2"))
 
-        
+    def test_name_with_other_characters(self):
+        self.assertTrue(name_linter.compare("owlcoding", "owl.coding"))
+        self.assertTrue(name_linter.compare("dôg", "dog"))
+        self.assertTrue(name_linter.compare("owlé", "owle"))
+        self.assertTrue(name_linter.compare("owlè", "owle"))
+        self.assertFalse(name_linter.compare("owl", "dog.dog"))
+
+    def test_name_with_similar_looks(self):
+        self.assertTrue(name_linter.compare("The Coding Owl", "owl.coding1"))
+        self.assertFalse(name_linter.compare("Super Extra Dog", "Lame Under Dog"))
+        self.assertTrue(name_linter.compare("Maté Owly Code", "mowlyco1"))
+        self.assertFalse(name_linter.compare("Super Extra Dog", "Maté Owly Code"))
