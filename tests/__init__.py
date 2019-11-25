1c6f9c09c1bce578d9285b835e3b755f49462624
sylhare <sylhare@outlook.com>
2019-11-25 17:39:08 -0500

    Update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -21,18 +21,17 @@ project B/
 TEST_LIST_PROJECTS = [
     'project A/\n   122\thero\n   100\tdog\n    29\towl\n     6\tloco\n     6\tmonkey\n     3\tcoder\n     1\tspy',
     'project B/\n    12\tdog\n    10\towl\n     5\towl2\n     2\tcoder']
-TEST_DICT_PROJECTS = {
-    'project A': [['122', 'hero'], ['100', 'dog'], ['29', 'owl'], ['6', 'loco'], ['6', 'monkey'], ['3', 'coder'],
-                  ['1', 'spy']],
-    'project B': [['12', 'dog'], ['10', 'owl'], ['5', 'owl2'], ['2', 'coder']]}
-TEST_VALUES_PROJECTS = [('122', '100', '29', '6', '6', '3', '1'),
-                        ('hero', 'dog', 'owl', 'loco', 'monkey', 'coder', 'spy')]
-TEST_DF_PROJECTS = """  project A    name project B
-0       122    hero       NaN
-1       100     dog        12
-2        29     owl        10
-3         6    loco       NaN
-4         6  monkey       NaN
-5         3   coder         2
-6         1     spy       NaN
-7       NaN    owl2         5"""
+TEST_DICT_PROJECTS = {'project A': [['hero', 122], ['dog', 100], ['owl', 29], ['loco', 6], ['monkey', 6], ['coder', 3],
+                                    ['spy', 1]],
+                      'project B': [['dog', 12], ['owl', 10], ['owl2', 5], ['coder', 2]]}
+TEST_VALUES_PROJECTS = [('hero', 'dog', 'owl', 'loco', 'monkey', 'coder', 'spy'),
+                        (122, 100, 29, 6, 6, 3, 1)]
+TEST_DF_PROJECTS = """     name  project A  project B
+0    hero      122.0        0.0
+1     dog      100.0       12.0
+2     owl       29.0       10.0
+3    loco        6.0        0.0
+4  monkey        6.0        0.0
+5   coder        3.0        2.0
+6     spy        1.0        0.0
+7    owl2        0.0        5.0"""
