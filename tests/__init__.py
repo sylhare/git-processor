edfb581fa298e0bd3a4fd21a765dc77e2e32277d
sylhare <sylhare@outlook.com>
2019-12-04 00:47:12 -0500

    Update demo

--- tests/__init__.py
+++ tests/__init__.py
@@ -58,7 +58,7 @@ TEST_TOTAL_PROJECTS = """       name  total
 5       spy   54.0
 6  thecoder    6.0"""
 
-TEST_PERCENTAGE_PROJECTS = """          total  total %
+TEST_PERCENTAGE_USER = """          total  total %
 name                    
 dog       210.0    44.30
 hero      122.0    25.74
@@ -68,7 +68,7 @@ owl        44.0     9.28
 spy        54.0    11.39
 thecoder    6.0     1.27"""
 
-TEST_PERCENTAGE_PROJECT = """          project B  total %
+TEST_PERCENTAGE_USER_PROJECT = """          project B  total %
 name                        
 dog            12.0    41.38
 hero            0.0     0.00
@@ -78,7 +78,7 @@ owl            15.0    51.72
 spy             0.0     0.00
 thecoder        2.0     6.90"""
 
-TEST_AVERAGE_PROJECT = """          total  average
+TEST_AVERAGE_USER = """          total  average
 name                    
 dog       210.0       70
 hero      122.0       40
@@ -87,3 +87,26 @@ monkey      6.0        2
 owl        44.0       14
 spy        54.0       18
 thecoder    6.0        2"""
+
+TEST_CONTRIBUTORS = """           contributors
+project A             7
+project B             3
+project C             4"""
+
+TEST_PERCENTAGE_PROJECT = """name         dog   hero  locom  monkey   owl   spy  thecoder  total  total %
+project A  100.0  122.0    6.0     6.0  29.0   1.0       3.0  267.0    56.33
+project B   12.0    0.0    0.0     0.0  15.0   0.0       2.0   29.0     6.12
+project C   98.0    0.0   26.0     0.0   0.0  53.0       1.0  178.0    37.55
+total      210.0  122.0   32.0     6.0  44.0  54.0       6.0  474.0   100.00"""
+
+TEST_AVERAGE_PROJECT = """name         dog   hero  locom  monkey   owl   spy  thecoder  total  average
+project A  100.0  122.0    6.0     6.0  29.0   1.0       3.0  267.0       33
+project B   12.0    0.0    0.0     0.0  15.0   0.0       2.0   29.0        3
+project C   98.0    0.0   26.0     0.0   0.0  53.0       1.0  178.0       22
+total      210.0  122.0   32.0     6.0  44.0  54.0       6.0  474.0       59"""
+
+TEST_FILTERED_DF = """        project A  project B
+name                        
+dog         100.0       12.0
+owl          29.0       10.0
+monkey        6.0        0.0"""
